8ae99c9c8903406b5c10abe81b590087c6cee751
Ricardoo <angelricardoglezsoto@gmail.com>
2024-11-25 08:09:57 -0600

    Ranas Saltarinas - fix: Invalid movements are no longer counted as attempts, and the size of the frogs has been corrected

--- RanasSaltarinas.cpp
+++ RanasSaltarinas.cpp
@@ -13,16 +13,16 @@ int main(){
     scanf("%d", &tamano);
     tamano = tamano*2+1;
     char pos[tamano], objetivo[tamano]; // Declarar arreglos, uno inicial y otro final para evaluar el ciclo
-
+    
     llenarArreglo(pos,objetivo,tamano); // Llamar a la funcion que llena los arreglos dependiendo el número de ranas ingresadas 
     ranasFuncion(pos,objetivo,tamano); // Llamar a la funcion que mueve a las ranas de posicion
-    
+
     return 0;
 }
 
 bool comparar(char pos[], char objetivo[], int tamano){ // Funcion que compara que el arreglo inicial sea igual que el final
-    for(int i=0; i<tamano; i++){ // For que recorre todas las posiciones del arreglo
-        if(pos[i]!=objetivo[i]){ // If que revisa si la posición en i es diferente en los dos arreglos, si es diferente retorna- 
+    for (int i=0; i<tamano; i++){ // For que recorre todas las posiciones del arreglo
+        if (pos[i]!=objetivo[i]){ // If que revisa si la posición en i es diferente en los dos arreglos, si es diferente retorna- 
             return false;       // false y termina de evaluar
         }
     }
@@ -38,7 +38,7 @@ void llenarArreglo(char pos[], char objetivo[], int tamano){ // Funcion para lle
         else if (i==(tamano/2)){ // If para buscar la mitad del array, si i es igual a la mitad de tamano se le asigna un espacio
             pos[i]=' ';
             objetivo[i]=' ';
-        } else{
+        } else {
             pos[i]='R'; // Asignar valores a las posiciones restantes
             objetivo[i]='A';
         }
@@ -47,52 +47,62 @@ void llenarArreglo(char pos[], char objetivo[], int tamano){ // Funcion para lle
 
 void ranasFuncion(char pos[], char objetivo[], int tamano){ // Funcion para mover las ranas de posicion
     int  rana, intentos=0;
-    while (!comparar(pos, objetivo, tamano)){ // While para que sea infinito con la funcion comparar negada, para que cuando retorne false la función comparar no termine el bucle 
+    do{
         imprimirRanas(pos, tamano);
         printf("\nElige posicion (1-%d): ",tamano); // Elegir la casilla de la rana a cambiar
         scanf("%d", &rana);
-        intentos++; // Contador de intentos
         printf("\n");
         rana--; // Restarle 1 a la casilla de la rana ingresada para que coincida con la posicion del arreglo
-    
-        if (pos[rana]== ' '){ // Verificar que la posicion de la rana ingresada no sea un espacio
-            printf("Movimiento invalido\n");
-        }
+
         if (pos[rana+1] == ' '){ // Verificar que exista un espacio en la siguiente posicion de la rana ingresada
             pos[rana+1] = pos[rana]; // Si hay espacio, asignar el valor de la posicion de la rana ingresada a la siguiente posicion
             pos[rana] = ' '; // Asignar el espacio nuevamente en la casilla ingresada ya que fue reemplazado por el valor de la rana ingresada
+            intentos++;
         } 
         else if (pos[rana-1] == ' '){ // Verificar que exista un espacio en la posicion anterior de la rana ingresada
             pos[rana-1] = pos[rana];
             pos[rana] = ' ';
+            intentos++;
         } 
         else if (pos[rana+1]!=pos[rana] && pos[rana+2] == ' '){ // Verificar que en la siguiente posicion de la rana ingresada no tenga el mismo-
             pos[rana+2] = pos[rana]; // valor (mismo color) y verificar que exista un espacio en las siguientes dos posiciones para que la rana
             pos[rana] = ' '; // pueda saltar y reemplazar su valor 
+            intentos++;
         }
         else if (pos[rana-1]!=pos[rana] && pos[rana-2] == ' '){ // Lo mismo pero con la posicion anterior 
             pos[rana-2] = pos[rana];
             pos[rana] = ' ';
+            intentos++;
         }
         else { 
             printf("Movimiento invalido\n"); // Cualquier otro movimiento va a ser invalido
         }
-    } // fin del while
-
+    }
+    while (!comparar(pos, objetivo, tamano));
+    
+    imprimirRanas(pos, tamano);
     printf("\nHas ganado en %d intentos.", intentos); 
 }
 
 void imprimirRanas(char pos[], int tamano){
+    int cuadradoTamano=8;
     const char *bloque = "\u2588\u2588"; // Declarar constante con el unicode 2588 que es el ascii 219 para poder imprimir cuadritos
-    
-    for (int i=1; i<=8; i++){ // For que recorre las filas del cuadrado
+    if (tamano==11){
+        cuadradoTamano=7;
+    } else if (tamano==13){
+        cuadradoTamano=6;
+    } else if (tamano==15){
+        cuadradoTamano=5;
+    }
+
+    for (int i=1; i<=cuadradoTamano; i++){ // For que recorre las filas del cuadrado
         for (int nVeces=0; nVeces<tamano; nVeces++){ // For que hace que se impriman los n cuadrados del tamaño ingresado pegados en la misma linea
-            for (int j=1; j<=8; j++){ // For que recorre las columnas del cuadro
-                if (i==1 || i==8 || j==1 || j==8){ // If que selecciona las orillas del cuadrado
+            for (int j=1; j<=cuadradoTamano; j++){ // For que recorre las columnas del cuadro
+                if (i==1 || i==cuadradoTamano || j==1 || j==cuadradoTamano){ // If que selecciona las orillas del cuadrado
                     printf("\x1b[38;5;11m%s", bloque); // Poner marco amarillo
                 }
                 else{
-                    if ((i==2 || i==7) && (j==2 || j==7)){ // Quitar esquinas del cuadrado interno
+                    if ((i==2 || i==cuadradoTamano-1) && (j==2 || j==cuadradoTamano-1)){ // Quitar esquinas del cuadrado interno
                         printf("\x1b[38;5;11m%s", bloque); // Pintar las esquinas de amarillo
                     }
                     else{
